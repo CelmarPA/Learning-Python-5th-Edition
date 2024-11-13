43f06f23c6ef4020125e33a19543e0820d6609ae
Celmar Pereira de Andrade <celmarpereiradeandrade@gmail.com>
2024-11-13 16:30:04 -0300

    End of chapter 3

--- Chapter 3 - How You Run Programs/script1.py
+++ Chapter 3 - How You Run Programs/script1.py
@@ -4,4 +4,6 @@ print(sys.platform)
 print(2 ** 100)             # Raise 2 to a power
 x = "Spam!"
 print(x * 8)                # String repetition
+print(2 ** 16)
+print(2 ** 32)
 input("Press enter to exit")                     #<== ADDED
